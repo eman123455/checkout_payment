6c849e0638f53498a3ee4f5516fd6fe751102b5f
Eman Kamel <kamele432@gmail.com>
2026-04-17 16:42:39 +0200

    add mycart routes

--- lib/core/resources/app_routes.dart
+++ lib/core/resources/app_routes.dart
@@ -1,14 +1,20 @@
 import 'package:checkout_payment/features/checkout/presentation/views/checkout_view.dart';
+import 'package:checkout_payment/features/checkout/presentation/views/my_cart_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoutes {
-  static const kCheckoutView='/';
+  static const kCheckoutView='CheckoutView';
+  static const kMyCartView='/';
   static GoRouter routes = GoRouter(
     routes: [
       GoRoute(
         path: kCheckoutView,
         builder: (context,state)=>CheckoutView()
-        )
+        ),
+      GoRoute(
+        path: kMyCartView,
+        builder: (context,state)=>MyCartView()
+        ),
     ]
     );
 }
