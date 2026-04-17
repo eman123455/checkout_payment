b8c67fe32f6999cf9b30c348f065fbbc124ef74f
Eman Kamel <kamele432@gmail.com>
2026-04-17 17:18:22 +0200

    add back arrow as svg

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:checkout_payment/core/resources/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -8,8 +9,10 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
+        elevation: 0,
         title: Text('Cart',style: AppTextStyles.style25),
         centerTitle: true,
+        leading: Center(child: SvgPicture.asset('assets/images/arrow.svg')),
       ) ,
       );
   }
