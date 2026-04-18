5df7ca5ef3988ea24523202d14acb11b4a425e0a
Eman Kamel <kamele432@gmail.com>
2026-04-18 15:41:09 +0200

    edit custom button

--- lib/core/components/custom_button.dart
+++ lib/core/components/custom_button.dart
@@ -3,21 +3,20 @@ import 'package:checkout_payment/core/resources/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text});
+  const CustomButton({super.key, required this.text, this.onPressed});
   final String text;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.symmetric(horizontal: 22,vertical: 20),
+          padding: EdgeInsets.symmetric(horizontal: 22, vertical: 20),
           backgroundColor: AppColors.kGreen,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8)
-          )
-          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
         child: Text(text, style: AppTextStyles.style24),
       ),
     );
