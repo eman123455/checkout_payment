44c828e03f69c743af9b51c1545a5ccd765e7e49
Eman Kamel <kamele432@gmail.com>
2026-04-18 15:04:08 +0200

    create app colors

--- lib/core/resources/app_colors.dart
+++ lib/core/resources/app_colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 
 class AppColors {
   static const kGreen = Color(0xff34A853);
+  static const kWhite = Colors.white;
+  static const kBlack = Colors.black;
 }
