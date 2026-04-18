1902faab15dc65bf1965f3108b4907479f2bab11
Eman Kamel <kamele432@gmail.com>
2026-04-18 06:29:16 +0200

    add screen utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:checkout_payment/core/resources/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const CheckoutApp());
@@ -9,9 +10,14 @@ class CheckoutApp extends StatelessWidget {
   const CheckoutApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-     debugShowCheckedModeBanner: false,
-     routerConfig: AppRoutes.routes,
+    return ScreenUtilInit(
+        designSize: const Size(375, 812),
+  minTextAdapt: true,
+  splitScreenMode: true,
+      builder: (context, child) => MaterialApp.router(
+       debugShowCheckedModeBanner: false,
+       routerConfig: AppRoutes.routes,
+      ), 
     );
   }
 }
