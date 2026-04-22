ded5db58cff1a7532a8749f5cb205b28f4f8f97c
Eman Kamel <kamele432@gmail.com>
2026-04-22 04:57:12 +0200

    create custom payment card

---
+++ lib/features/checkout/presentation/widgets/custom_credit_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key, required this.formKey, required this.autovalidateMode});
+  final GlobalKey<FormState> formKey;
+  final AutovalidateMode autovalidateMode;
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          isHolderNameVisible: true,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        ),
+        CreditCardForm(
+          autovalidateMode: widget.autovalidateMode,
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          onCreditCardModelChange: (creditcardmodel) {
+            setState(() {
+              cardNumber = creditcardmodel.cardNumber;
+              expiryDate = creditcardmodel.expiryDate;
+              cardHolderName = creditcardmodel.cardHolderName;
+              cvvCode = creditcardmodel.cvvCode;
+              showBackView = creditcardmodel.isCvvFocused;
+            });
+          },
+          formKey: widget.formKey,
+        ),
+      ],
+    );
+  }
+}
