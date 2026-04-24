5630e2d2f39157a09f79de1baff1ff3ae63340c7
Eman Kamel <kamele432@gmail.com>
2026-04-25 01:26:03 +0300

    edit custom appbar

--- lib/core/components/custom_app_bar.dart
+++ lib/core/components/custom_app_bar.dart
@@ -16,7 +16,11 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       title: Text(title, style: AppTextStyles.style25),
       centerTitle: true,
-      leading: Center(child: SvgPicture.asset(Images.arrowImagePath)),
+      leading: Center(child: IconButton(
+  icon: SvgPicture.asset(Images.arrowImagePath),
+  onPressed: () {
+  },
+),),
     );
   }
 }
