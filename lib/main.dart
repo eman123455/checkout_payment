725731178f5b075fae3d5d7b9841eab24435f201
Eman Kamel <kamele432@gmail.com>
2026-05-19 12:44:52 +0300

    create service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:checkout_payment/core/resources/app_routes.dart';
+import 'package:checkout_payment/core/service_locator/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
+  startServiceLocator();
   runApp(const CheckoutApp());
 }
 
@@ -11,13 +13,13 @@ class CheckoutApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(375, 812),
-  minTextAdapt: true,
-  splitScreenMode: true,
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
       builder: (context, child) => MaterialApp.router(
-       debugShowCheckedModeBanner: false,
-       routerConfig: AppRoutes.routes,
-      ), 
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRoutes.routes,
+      ),
     );
   }
 }
